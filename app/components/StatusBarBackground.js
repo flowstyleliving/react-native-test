94fb237caa7c8d8c4728f5dd0c7ca9e883ceea08
flowstyleliving <mys628@gmail.com>
2016-05-18 15:39:49 -0700

    initial commit

---
+++ app/components/StatusBarBackground.js
@@ -0,0 +1,21 @@
+'use strict'
+import React, { Component, View } from 'react-native'
+
+class StatusBarBackground extends View {
+  render() {
+    return (
+      <View style={[styles.statusBarBackground, this.props.style || {}]}>
+      </View>
+    )
+  }
+}
+
+const styles = React.StyleSheet.create({
+
+  statusBarBackground: {
+    height: 20,
+    backgroundColor: "white"
+  }
+})
+
+module exports = StatusBarBackground
